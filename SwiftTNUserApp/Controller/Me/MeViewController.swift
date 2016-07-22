b380bac7dd989af03fab65744472cdc5a8331765
wangxiaobai1840 <1510517369@qq.com>
2016-07-22 19:21:03 +0800

    gengxin

--- SwiftTNUserApp/Controller/Me/MeViewController.swift
+++ SwiftTNUserApp/Controller/Me/MeViewController.swift
@@ -12,6 +12,9 @@ class MeViewController: RootViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let meView  = MeView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 120))
+        self.view.addSubview(meView)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- SwiftTNUserApp/View/Me/MeView.swift
+++ SwiftTNUserApp/View/Me/MeView.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class MeView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    var bg_img:UIImageView?
+  override  init(frame: CGRect) {
+    self.init()
+    bg_img = UIImageView.init(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 120))
+    bg_img?.image = UIImage(named:"bg_image")
+    self.addSubview(bg_img!)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
